6df09a1be32034e4e3a3a753ac1cc1ceca53e5b5
Hysios Hu <hysios@gmail.com>
2014-07-23 17:29:56 +0800

    complete generate sprites files

--- index.js
+++ index.js
@@ -4,21 +4,52 @@ var Writer = require('broccoli-writer'),
 
 module.exports = BroccoliSpritesmith;
 BroccoliSpritesmith.prototype = Object.create(Writer.prototype);
-BroccoliSpritesmith.prototype.constructor = MyCompiler;
+BroccoliSpritesmith.prototype.constructor = BroccoliSpritesmith;
 function BroccoliSpritesmith (arg1, arg2, ...) {
-  if (!(this instanceof BroccoliSpritesmith)) return new BroccoliSpritesmith(arg1, arg2, ...);
-  ...
+  if (!(this instanceof BroccoliSpritesmith)) return new BroccoliSpritesmith(inputTree, options);
+  options = options || {};
+
+  this.options = defaults(options, {
+    srcDir: 'public',
+    rootPath: 'public',
+    spritesPath: 'assets/sprites',
+    spriteName: 'sprites_background',
+    ext: 'png',
+    engine: 'auto',
+    algorithm: 'top-down',
+    padding: 0
+  });
+
+  this.inputTree = inputTree;
+
 };
 
 BroccoliSpritesmith.prototype.write = function (readTree, destDir) {
-  ...
+  var self = this;
+  var ext = this.options.ext || '.png';
+  var spritesPath = this.options.spritesPath;
+  var rootPath = path.join(process.cwd(), this.options.rootPath);
+  var spriteImagePath = path.join(rootPath, spritesPath);
+  var spritesImages = walkSync(spriteImagePath).map(function(file){
+    return path.join(spriteImagePath, file);
+  });
+  var spriteName = this.options.spriteName;
+  var ext = this.options.ext;
+  var spritesOutputFile = path.join(destDir, spritesPath, spriteName + '.' + ext);
+
+  var runOptions = {
+    src: spritesImages
+  };
+
+  return new Promise(function(resolve, reject){
+    spritesmith(runOptions, function(err, result){
+      if (err) {
+        reject(err);
+      } else {
+        mkdirp.sync(path.join(destDir, spritesPath));
+        fs.writeFileSync(spritesOutputFile, result.image, 'binary');
+        resolve(destDir);
+      }
+    });
+  });
 };
-
-
-
-var sprites = ['sprite1.png', 'sprite2.jpg', 'sprite3.png'];
-Spritesmith({'src': sprites}, function (err, result) {
-  result.image; // Binary string representation of image
-  result.coordinates; // Object mapping filename to {x, y, width, height} of image
-  result.properties; // Object with metadata about spritesheet {width, height}
-});
